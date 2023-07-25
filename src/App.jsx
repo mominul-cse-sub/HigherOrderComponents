7d6188404aa6e8c575b9dc071776bf6f35d278f3
MD MOMINUL ISLAM <mdmominulislam167@gmail.com>
2023-07-25 10:10:56 +0600

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ClickCounter from './components/Clickcounter'
+import ClickCounter from './components/ClickCounter'
 import HoverCounter from './components/HoverCounter'
 
 function App() {
